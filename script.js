d467ddb3f1e1f709f4f678c5724173488593ed3d
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2023-01-19 21:41:53 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function NotFoundError() {
+
+    //let LoginButton = document.getElementById('Login')
+
+    let UserNameText = document.getElementById('UserName')
+    let PasswordText = document.getElementById('Password')
+
+    if(UserNameText.value == 'Sakamochan' && PasswordText.value == '15')
+    {
+        //URL
+        var browse = 'https://github.com/Pop-Apple'
+        window.location.href = browse
+    }
+    else
+    {
+        var Message = "不明なアドレスです。\n\n再度入力情報を確認して下さい。\n\nDevelop Pop-Apple"
+        alert(Message)
+
+        //Reset
+        UserNameText.value = null
+        PasswordText.value = null
+    }
+}
